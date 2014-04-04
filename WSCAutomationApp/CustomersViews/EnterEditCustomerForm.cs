de7bb788381fb27e0a9313e61ab78c08101f6178
Colby Gauf <colby.gauff@gmail.com>
2014-04-03 21:11:58 -0500

    **Created EnterEditCustomerForms

---
+++ WSCAutomationApp/CustomersViews/EnterEditCustomerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WSCAutomation.App
+{
+    public partial class EnterEditCustomerForm : WSCAutomation.App.EnterEditRecordFormBase
+    {
+        public EnterEditCustomerForm()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
