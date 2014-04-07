21004d407e506a6d17102f7c75ba282621a33260
Chris Newby <cnewby2@verizon.net>
2014-04-07 11:53:44 -0500

    * Corrected Inventory.cs for correct property casing (Pascal) * Removed constructors from Inventory.cs as they are not needed

--- WSCAutomationLib/Inventory/Inventory.cs
+++ WSCAutomationLib/Inventory/Inventory.cs
@@ -4,40 +4,14 @@ namespace WSCAutomation.Inventory
 {
     public class Inventory
     {
-        public int inventoryID { get; set; }
-        public string itemName { get; set; }
-        public string manufacturer { get; set; }
-        public int quantity { get; set; }
+        public int InventoryID { get; set; }
+        public string ItemName { get; set; }
+        public string Manufacturer { get; set; }
+        public int Quantity { get; set; }
 
         public Inventory()
         {
         }
 
-        // This is for searching by ID and will create an object from database returned results
-        // Not sure of we need this...
-        public Inventory(int inventoryIDIn)
-        {
-            inventoryID = inventoryIDIn;
-            // TODO Call to database to determine matching ID, return results into local variables
-            // itemName = return from database query
-            // quantity = return from database query
-        }
-
-        // This constructor is for creating an object from database results when searched from itemName
-        // Again, not sure we'll need this...
-        public Inventory(string itemNameIn)
-        {
-            itemName = itemNameIn;
-            // TODO Call to database to search inventory by Item Name
-            // inventoryID = return from database query
-            // quantity = return from database query
-        }
-
-        public Inventory(int inventoryIDIn, string itemNameIn, int quantityIn)
-        {
-            inventoryID = inventoryIDIn;
-            itemName = itemNameIn;
-            quantityIn = quantity;
-        }
     }
 }
