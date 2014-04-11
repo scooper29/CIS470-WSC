e952edc2b46fa5a0938745bc2f594d3467f170fc
Jason Gray <nerd131@gmail.com>
2014-04-11 16:35:12 -0400

    * Rough draft of what will happen when save is clicked in the PurchaseInventoryRequest class

--- WSCAutomationApp/InventoryViews/PurchaseInventoryRequest.cs
+++ WSCAutomationApp/InventoryViews/PurchaseInventoryRequest.cs
@@ -18,6 +18,111 @@ namespace WSCAutomation.App
 
         private void btnSave_Click(object sender, EventArgs e)
         {
+            // Rough logic, will clean up when related areas of database access completed.
+            // Declare
+            DateTime orderDate;
+            DateTime deliveryDate;
+
+            decimal inventoryQTY;
+
+            string inventoryID;
+
+
+            // Initialize
+            inventoryID = txbInvReqID.Text;
+            inventoryQTY = upDownInventoryQty.Value;
+
+            orderDate = dateTimePickerOrderDate.Value;
+            deliveryDate = dateTimePickerDelieveryDate.Value;
+
+            // Validate
+
+            // Make sure entered inventory ID is the right length
+            if (inventoryID.Length != 4)
+            {
+                MessageBox.Show(this
+                    , "Invalid inventory ID length. Please enter a four digit inventory ID."
+                    , "Inventory ID length error"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                return;
+            }
+
+            // Make sure the entered inventory ID is an integer
+            try
+            {
+                int i = int.Parse(inventoryID);
+            }
+            catch
+            {
+                MessageBox.Show(this
+                    , "Invalid inventory ID. Please enter a four digit inventory ID."
+                    , "Invalid inventory ID error"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                return;
+            }
+
+            // make sure delivery date is later than the order date
+            if (orderDate >= deliveryDate)
+            {
+                MessageBox.Show(this
+                    , "Please make sure the delivery date comes after the order date."
+                    , "Invalid order/delivery date error"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                return;
+            }
+            
+            // Mainline
+
+            // Create email
+            MailAddress from = new MailAddress("Clerk@WSC.com");
+            MailAddress to = new MailAddress("Specialist@WSC.com");
+            MailMessage message = new MailMessage(from, to);
+
+            message.Subject = "Inventory order purchase made";
+            message.Body = @"The order you requested for inventory ID " 
+                + inventoryID
+                + "has been ordered from our supplier. Details below." 
+                + System.Environment.NewLine
+                + System.Environment.NewLine
+                + "Inventory ID: "
+                + inventoryID
+                + System.Environment.NewLine
+                + "Quantity Ordered: "
+                + inventoryQTY
+                + System.Environment.NewLine
+                + "Order Date: "
+                + orderDate
+                + System.Environment.NewLine
+                + "Expected Delivery Date: "
+                + deliveryDate;
+
+            SmtpClient client = new SmtpClient(server); //NEED SERVER
+
+            // Attempt to send email
+            try
+            {
+                client.Send(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this
+                    , "Exception caught while attempting to send email in btnSave_Click(): {0}"
+                    , "email send error"
+                    , MessageBoxButtons.OK
+                    , MessageBoxIcon.Error);
+
+                return;
+            }
+
+            // Format Results
+            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
+
 
             /*if dateTimePickerOrderDate >= dateTimePickerDelieveryDate
              * then error "InVaild Delievery Date"
@@ -38,7 +143,6 @@ namespace WSCAutomation.App
              * 
              * smtpClient.Send(mailMsg);
             */
-
         }   
     };
 }
