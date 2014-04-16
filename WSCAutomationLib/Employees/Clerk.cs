602acbabcc0cf8c8338a55530dea03e68cec1d2d
Jason Gray <nerd131@gmail.com>
2014-04-16 18:09:03 -0400

    * Added Inventory Order Procedure that accepts the class object * Edited out the validation process for the Inventory Order Procedure that accepts values passed by value * Edited the Email procedures for the Inventory Order Procedure that accepts values passed by value

--- WSCAutomationLib/Employees/Clerk.cs
+++ WSCAutomationLib/Employees/Clerk.cs
@@ -46,136 +46,39 @@ namespace WSCAutomation.Employees
 
 
 #region OrderInventory
+        #region OrderInventory by passed class
         /// <summary>
         /// Procedure for taking the parameters from the PurchaseInventoryRequest class,
         /// sending an email to the appropriate party, and updating the database.
         /// </summary>
-        /// <param name="orderDate">Order date entered in PurchaseInventoryRequest.cs</param>
-        /// <param name="deliveryDate">
-        /// Expected delivery date entered in PurchaseInventoryRequest.cs
-        /// </param>
-        /// <param name="inventoryQTY">
-        /// Inventory Quantity entered in PurchaseInventoryRequest.cs
-        /// </param>
-        /// <param name="inventoryID">Inventory ID entered in PurchaseInventoryRequest.cs
-        /// </param>
-        private void OrderInventoryProcedure(DateTime orderDate, DateTime deliveryDate
-            ,decimal inventoryQTY, string inventoryID)
+        /// <param name="invOrder">InventoryOrder Class Object</param>
+        public void OrderInventoryProcedure(Inventory.InventoryOrder invOrder)
         {
-            // Rough logic, will clean up when related areas of database access completed.
             // Declare
-            bool validationStringToInt;
-            bool validationStringLength;
-            bool validationOrderDates;
+            // N/A
 
             // Initialize
-            validationStringLength = false;
-            validationStringToInt = false;
-            validationOrderDates = false;
+            // N/A
 
             // Validate
-            validationStringLength = StringLengthValidation(inventoryID.Length);
-            validationStringToInt = StringToIntegerValidation(inventoryID);
-            validationOrderDates = DateValidation(orderDate, deliveryDate);
-
-            if (validationStringLength == false || validationStringToInt == false
-                || validationOrderDates == false)
-            {
-                return;
-            }
+            // N/A
 
             // Mainline
-            InventoryId = int.Parse(inventoryID);
-            Quantity = (int)inventoryQTY;
-            OrderDate = orderDate;
-            ArrivalDate = deliveryDate;
-
-            EmailDepartment();
+            EmailDepartment(invOrder);
+            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
 
             // Format Results
+            // N/A
+
             // Return
-            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
+            // N/A
         }
 
         /// <summary>
-        /// Accepts int value, makes sure it is a four digit integer, returns validation
-        /// success result.
+        /// Creates and sends email using properties in InventoryOrder
         /// </summary>
-        /// <param name="stringLengthToValidate">
-        /// An int to validate is a four digit integer
-        /// </param>
-        /// <returns>Bool value stating whether validation successful.</returns>
-        private bool StringLengthValidation (int stringLengthToValidate)
-        {
-            if (stringLengthToValidate != 4)
-            {
-                //MessageBox.Show(this
-                //    , "Invalid inventory ID length. Please enter a four digit inventory ID."
-                //    , "Inventory ID length error"
-                //    , MessageBoxButtons.OK
-                //    , MessageBoxIcon.Error);
-
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// accepts string value, checks to see if it is an integer, return validation
-        /// results.
-        /// </summary>
-        /// <param name="stringToValidate">
-        /// String to check if characters are all integers.
-        /// </param>
-        /// <returns>Bool value stating whether validation successful.</returns>
-        private bool StringToIntegerValidation (string stringToValidate)
-        {
-            try
-            {
-                int i = int.Parse(stringToValidate);
-
-                return true;
-            }
-            catch
-            {
-                //MessageBox.Show(this
-                //    , "Invalid inventory ID. Please enter a four digit inventory ID."
-                //    , "Invalid inventory ID error"
-                //    , MessageBoxButtons.OK
-                //    , MessageBoxIcon.Error);
-
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Accepts two DateTime values, makes sure the first date is not greater than or
-        /// equal to the second date, returns validation results.
-        /// </summary>
-        /// <param name="firstDate">The date that comes first chronologically</param>
-        /// <param name="secondDate">The date that comes second chronologically</param>
-        /// <returns>Bool value stating whether validation successful.</returns>
-        private bool DateValidation (DateTime firstDate, DateTime secondDate)
-        {
-            if (firstDate >= secondDate)
-            {
-                //MessageBox.Show(this
-                //    , "Please make sure the delivery date comes after the order date."
-                //    , "Invalid order/delivery date error"
-                //    , MessageBoxButtons.OK
-                //    , MessageBoxIcon.Error);
-
-                return false;
-            }
-
-            return true;
-        }
-
-        /// <summary>
-        /// Creates and sends the Email.
-        /// </summary>
-        private void EmailDepartment()
+        /// <param name="invOrder">InventoryOrder class object</param>
+        private void EmailDepartment(Inventory.InventoryOrder invOrder)
         {
             // Create Email
             var fromAddress = new MailAddress("wscclerk60683@gmail.com"
@@ -185,21 +88,21 @@ namespace WSCAutomation.Employees
             const string FROM_PASSWORD = "senprojCIS470";
             const string SUBJECT = "Inventory order purchase made";
             string body = "The order you requested for inventory ID "
-                + InventoryId
+                + invOrder.InventoryId
                 + "has been ordered from our supplier. Details below."
                 + System.Environment.NewLine
                 + System.Environment.NewLine
                 + "Inventory ID: "
-                + InventoryId
+                + invOrder.InventoryId
                 + System.Environment.NewLine
                 + "Quantity Ordered: "
-                + Quantity
+                + invOrder.Quantity
                 + System.Environment.NewLine
                 + "Order Date: "
-                + OrderDate
+                + invOrder.OrderDate
                 + System.Environment.NewLine
                 + "Expected Delivery Date: "
-                + ArrivalDate;
+                + invOrder.ArrivalDate;
 
             // Use gmail SMTP client
             var smtp = new SmtpClient
@@ -223,6 +126,98 @@ namespace WSCAutomation.Employees
                 smtp.Send(message);
             }
         }
+        #endregion
+
+        #region OrderInventory by passed values. Outdated code not removed.
+        /// <summary>
+        /// Procedure for taking the parameters from the PurchaseInventoryRequest class,
+        /// sending an email to the appropriate party, and updating the database.
+        /// </summary>
+        /// <param name="orderDate">Order date entered in PurchaseInventoryRequest.cs</param>
+        /// <param name="deliveryDate">
+        /// Expected delivery date entered in PurchaseInventoryRequest.cs
+        /// </param>
+        /// <param name="inventoryQTY">
+        /// Inventory Quantity entered in PurchaseInventoryRequest.cs
+        /// </param>
+        /// <param name="inventoryID">Inventory ID entered in PurchaseInventoryRequest.cs
+        /// </param>
+        public void OrderInventoryProcedure(DateTime orderDate, DateTime deliveryDate
+            ,decimal inventoryQTY, string inventoryID)
+        {
+            // Declare
+            // N/A
+
+            // Initialize
+            // N/A
+
+            // Validate
+            // N/A
+
+            // Mainline
+            EmailDepartment(orderDate, deliveryDate, inventoryQTY, inventoryID);
+            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
+
+            // Format Results
+            // N/A
+
+            // Return
+            // N/A
+        }
+
+        /// <summary>
+        /// Creates and sends the Email.
+        /// </summary>
+        private void EmailDepartment(DateTime orderDate, DateTime deliveryDate
+            , decimal inventoryQTY, string inventoryID)
+        {
+            // Create Email
+            var fromAddress = new MailAddress("wscclerk60683@gmail.com"
+                , "WSC Clerk Automated");
+            var toAddress = new MailAddress("wscspec60683@gmail.com"
+                , "Specialist");
+            const string FROM_PASSWORD = "senprojCIS470";
+            const string SUBJECT = "Inventory order purchase made";
+            string body = "The order you requested for inventory ID "
+                + inventoryID
+                + "has been ordered from our supplier. Details below."
+                + System.Environment.NewLine
+                + System.Environment.NewLine
+                + "Inventory ID: "
+                + inventoryID
+                + System.Environment.NewLine
+                + "Quantity Ordered: "
+                + inventoryQTY
+                + System.Environment.NewLine
+                + "Order Date: "
+                + orderDate
+                + System.Environment.NewLine
+                + "Expected Delivery Date: "
+                + deliveryDate;
+
+            // Use gmail SMTP client
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, FROM_PASSWORD)
+            };
+
+            // Send message
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = SUBJECT
+                ,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+        #endregion
 #endregion
-	};
+    };
 }
