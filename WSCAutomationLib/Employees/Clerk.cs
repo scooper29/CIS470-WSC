25ec2467754c6b47e39e35461f54a1ad2cfcae69
Jason Gray <nerd131@gmail.com>
2014-04-17 16:42:07 -0400

    * Removed pass by value OrderInventory method as DBAddInventory requires pass by class. * Completed Pass by class OrderInventory method.

--- WSCAutomationLib/Employees/Clerk.cs
+++ WSCAutomationLib/Employees/Clerk.cs
@@ -46,13 +46,12 @@ namespace WSCAutomation.Employees
 
 
 #region OrderInventory
-        #region OrderInventory by passed class
         /// <summary>
         /// Procedure for taking the parameters from the PurchaseInventoryRequest class,
         /// sending an email to the appropriate party, and updating the database.
         /// </summary>
         /// <param name="invOrder">InventoryOrder Class Object</param>
-        public void OrderInventoryProcedure(Inventory.InventoryOrder invOrder)
+        public int OrderInventoryProcedure(Inventory.InventoryOrder invOrder)
         {
             // Declare
             // N/A
@@ -65,13 +64,12 @@ namespace WSCAutomation.Employees
 
             // Mainline
             EmailDepartment(invOrder);
-            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
 
             // Format Results
             // N/A
 
             // Return
-            // N/A
+            return Database.DatabaseManager.Instance.DBAddInventory(invOrder);
         }
 
         /// <summary>
@@ -126,98 +124,6 @@ namespace WSCAutomation.Employees
                 smtp.Send(message);
             }
         }
-        #endregion
-
-        #region OrderInventory by passed values. Outdated code not removed.
-        /// <summary>
-        /// Procedure for taking the parameters from the PurchaseInventoryRequest class,
-        /// sending an email to the appropriate party, and updating the database.
-        /// </summary>
-        /// <param name="orderDate">Order date entered in PurchaseInventoryRequest.cs</param>
-        /// <param name="deliveryDate">
-        /// Expected delivery date entered in PurchaseInventoryRequest.cs
-        /// </param>
-        /// <param name="inventoryQTY">
-        /// Inventory Quantity entered in PurchaseInventoryRequest.cs
-        /// </param>
-        /// <param name="inventoryID">Inventory ID entered in PurchaseInventoryRequest.cs
-        /// </param>
-        public void OrderInventoryProcedure(DateTime orderDate, DateTime deliveryDate
-            ,decimal inventoryQTY, string inventoryID)
-        {
-            // Declare
-            // N/A
-
-            // Initialize
-            // N/A
-
-            // Validate
-            // N/A
-
-            // Mainline
-            EmailDepartment(orderDate, deliveryDate, inventoryQTY, inventoryID);
-            // DATABASE ACCESS PROCEDURES FOR ORDERING INVENTORY COMPLETION REQUIRED
-
-            // Format Results
-            // N/A
-
-            // Return
-            // N/A
-        }
-
-        /// <summary>
-        /// Creates and sends the Email.
-        /// </summary>
-        private void EmailDepartment(DateTime orderDate, DateTime deliveryDate
-            , decimal inventoryQTY, string inventoryID)
-        {
-            // Create Email
-            var fromAddress = new MailAddress("wscclerk60683@gmail.com"
-                , "WSC Clerk Automated");
-            var toAddress = new MailAddress("wscspec60683@gmail.com"
-                , "Specialist");
-            const string FROM_PASSWORD = "senprojCIS470";
-            const string SUBJECT = "Inventory order purchase made";
-            string body = "The order you requested for inventory ID "
-                + inventoryID
-                + "has been ordered from our supplier. Details below."
-                + System.Environment.NewLine
-                + System.Environment.NewLine
-                + "Inventory ID: "
-                + inventoryID
-                + System.Environment.NewLine
-                + "Quantity Ordered: "
-                + inventoryQTY
-                + System.Environment.NewLine
-                + "Order Date: "
-                + orderDate
-                + System.Environment.NewLine
-                + "Expected Delivery Date: "
-                + deliveryDate;
-
-            // Use gmail SMTP client
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, FROM_PASSWORD)
-            };
-
-            // Send message
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = SUBJECT
-                ,
-                Body = body
-            })
-            {
-                smtp.Send(message);
-            }
-        }
-        #endregion
 #endregion
     };
 }
