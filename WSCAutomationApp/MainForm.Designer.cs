bc32c9647d54458f776e26c375928566c7df8525
Colby Gauf <colby.gauff@gmail.com>
2014-04-20 23:37:55 -0500

    ***Added the new log that Chris created to the background.

--- WSCAutomationApp/MainForm.Designer.cs
+++ WSCAutomationApp/MainForm.Designer.cs
@@ -180,6 +180,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WSCAutomation.App.Properties.Resources.WSCBackgroundSmall;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(784, 562);
             this.Controls.Add(this.mainMenu);
             this.IsMdiContainer = true;
--- WSCAutomationApp/Properties/Resources.Designer.cs
+++ WSCAutomationApp/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace WSCAutomation.App.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WSCBackgroundSmall {
+            get {
+                object obj = ResourceManager.GetObject("WSCBackgroundSmall", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
